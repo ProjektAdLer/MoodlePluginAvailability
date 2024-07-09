d561ec5f3024cb2ebeb0a49608ca586e9818f7e2
Markus Heck <markus.heck@hs-kempten.de>
2024-07-09 16:34:15 +0200

    minor improvements/cleanups/fixes

--- version.php
+++ version.php
@@ -10,8 +10,8 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2024060300;
 $plugin->requires = 2022112800;  // Moodle version
 $plugin->component = 'availability_adler';
-$plugin->release = '3.0.1';
-$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '3.0.1-dev';
+$plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = array(
     'local_logging' => ANY_VERSION,
 );
